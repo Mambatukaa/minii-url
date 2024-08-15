cf6632377f07ff82cde6917410e71fd1964d51fc
mambatukaa <sbatuka9@gmail.com>
2024-08-14 20:58:01 -0700

    add snowflake

--- server/internal/utils/codeGenerator.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-// Generate a code for the short URL
-func CodeGenerator(longUrl string) (string, error) {
-	// Code generator function
-
-	return "code234", nil
-}
---
+++ server/internal/utils/snowflake_test.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+var wg sync.WaitGroup // Remove the unused variable declaration
+
+// Rename the main function to a different name
+func TestSnowflake(t *testing.T) {
+	w := NewWorker(5, 5)
+
+	ch := make(chan uint64, 10000)
+	count := 10000
+	wg.Add(count)
+	defer close(ch)
+	// Concurrently count goroutines for snowFlake ID generation
+	for i := 0; i < count; i++ {
+		go func() {
+			defer wg.Done()
+			id, _ := w.NextID()
+			ch <- id
+		}()
+	}
+	wg.Wait()
+	m := make(map[uint64]int)
+	for i := 0; i < count; i++ {
+		id := <-ch
+		// If there is a key with id in the map, it means that the generated snowflake ID is duplicated
+		_, ok := m[id]
+		if ok {
+			fmt.Printf("repeat id %d\n", id)
+			return
+		}
+		// store id as key in map
+		m[id] = i
+		fmt.Println("snowflake ID:", id)
+	}
+	// successfully generated snowflake ID
+	fmt.Println("All", len(m), "snowflake ID Get successed!")
+
+}
