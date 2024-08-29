037eb7612c339ef8b4ed05d724b38f1595c0eaa6
mambatukaa <sbatuka9@gmail.com>
2024-08-29 14:44:16 -0700

    up

--- client/src/Seo.tsx
+++ client/src/Seo.tsx
@@ -2,16 +2,11 @@ import { Helmet } from 'react-helmet';
 
 const SEO = () => {
   const title = 'Minii URL Shortener';
-  const description = 'A simple URL shortener';
-  const imageUrl = '../assets/images/og.png';
 
   return (
     <Helmet>
       <title>{title}</title>
-      <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
     </Helmet>
   );
 };
