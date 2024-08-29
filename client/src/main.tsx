58f0694ac8a7f0c492b8dc2617a14808400aa799
mambatukaa <sbatuka9@gmail.com>
2024-08-29 14:46:53 -0700

    up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import SEO from './Seo.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SEO />
     <BrowserRouter>
       <App />
     </BrowserRouter>
