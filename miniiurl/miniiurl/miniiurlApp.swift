eb8236308a5b8ee5335a3bae5d58e8f8702712ae
mambatukaa <sbatuka9@gmail.com>
2024-09-03 19:13:51 -0700

    update icon

---
+++ miniiurl/miniiurl/miniiurlApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniiurlApp.swift
+//  miniiurl
+//
+//  Created by Mambatukaa on 2024.09.03.
+//
+
+import SwiftUI
+
+@main
+struct miniiurlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
